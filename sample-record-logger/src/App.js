298280bbd143f36ba4ca3370cac752778945e57c
Jamar Torres <Jetorres88@gmail.com>
2020-09-03 19:47:27 -0400

    change App component to class component

--- sample-record-logger/src/App.js
+++ sample-record-logger/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>SKU Sample record Logger</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      logs: null,
+      showModal: false,
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>SKU Sample record Logger</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
