c0c70aca3447cf35ba4e818510e9e4f833c11ef5
Jamar Torres <Jetorres88@gmail.com>
2020-09-03 20:18:41 -0400

    import data from json file, fetch data upon mounting component, render data using map method to listen values.

--- sample-record-logger/src/App.js
+++ sample-record-logger/src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import skuData from './SKUNinja-sample-logs.json';
 import './App.css';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      logs: null,
+      data: skuData,
       showModal: false,
     }
   }
+
+  // fetching data upon mounting the component
+  componentDidMount(){
+    fetch('./SKUNinja-sample-logs.json')
+    // here I 
+    .then((res) => res.json())
+    .then(data => this.setState({data: data}))
+    .catch(error => console.error(error))
+  }
+
+
   render() {
+    console.log('this.state', this.state)
     return (
       <div className="App">
         <h1>SKU Sample record Logger</h1>
+        {this.state.data.map(logs => (
+          <div>
+            {logs.subject}
+          </div>
+        ))}
       </div>
     );
   }
